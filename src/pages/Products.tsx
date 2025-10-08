c8d0f3894ac624fb1abe9991ac592624a6d906a6
Kesava Masanam <133848882+MasanamKesava@users.noreply.github.com>
2025-10-08 22:36:48 +0530

    Update Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -165,7 +165,7 @@ const Products = () => {
     { name: "Vite",           src: "https://cdn.simpleicons.org/vite" },
     { name: "React Router",   src: "https://cdn.simpleicons.org/reactrouter" },
     { name: "GitHub",         src: "https://cdn.simpleicons.org/github" },
-    { name: "Framer-Motion",  src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ejable.com%2Ftech-corner%2Fapplication-development-and-testing%2Fframer-motion-with-react%2F&psig=AOvVaw3ff_XvX__oXH_f29ongNdj&ust=1760028776480000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLCEkKOIlZADFQAAAAAdAAAAABAL"},
+    { name: "Framer-Motion",  src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F13820034-Framer-Motion-Logo&psig=AOvVaw0rH69bvIpTfwl4magK2ugf&ust=1760029525708000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCICbmoiLlZADFQAAAAAdAAAAABAU"},
     // VectorLogo.zone
     { name: "Netlify",        src: "https://www.vectorlogo.zone/logos/netlify/netlify-icon.svg" },
     { name: "VS Code",        src: "https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg" },
